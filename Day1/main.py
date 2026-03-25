b6e596c41c81a6017536b64109550cdda2037d81
TengKimhan <tengkimhan@gmail.com>
2026-03-25 23:12:40 +1030

    Day 1

---
+++ Day1/main.py
@@ -0,0 +1,23 @@
+# Day1
+# 1. Printing to the Console in Python
+print("Hello World")
+
+# 2. String Manipulation
+# 2.1. Escape characters
+print("Hello world\nHello world!\nHello world")
+
+# 2.2. String concatenation
+print("Hello" + " " + "Kimhan")
+
+# 3. The Python Input Function
+input("What is your name?")
+
+# 4. Python Variables
+username = input("What is your name?")
+print(username)
+print("Username length: ")
+print(len(username))
+
+# 4. Variable Naming
+# Description: Using snack_case
+last_name = "teng"
