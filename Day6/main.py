8fa79e90bd94b816b79b3b45cb617a940b34c67e
TengKimhan <tengkimhan@gmail.com>
2026-04-03 18:24:24 +1030

    Day 6

---
+++ Day6/main.py
@@ -0,0 +1,22 @@
+# Day6: Functions, Code Blocks and While Loops
+
+# 1. Functions
+# 1.1. Built-in Functions: https://docs.python.org/3/library/functions.html
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+# 1.2. Create or Define function
+def my_func():
+    print("This is my function")
+
+
+# 1.3. Calling function
+my_func()
+
+# 2. While Loops
+i = 5
+while i < 10:
+    print(i)
+    i += 1
